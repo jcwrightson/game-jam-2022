b43e4e5a5e5f2b37b4b46cf55c0ed0fc888a8eec
timslow1 <81386065+timslow1@users.noreply.github.com>
2022-05-19 13:25:31 +0100

    UI Text added (#6)
    UI Text overlay for Score and Rage added

--- Assets/Scripts/PlayerProgress.cs
+++ Assets/Scripts/PlayerProgress.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerProgress : MonoBehaviour
 {
+
     public int RageMeter;
 
     public int Score;
+
+    public Text ScoreText;
+
+    public Text RageText;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +30,18 @@ public class PlayerProgress : MonoBehaviour
     public void UpdateScore(int value)
     {
         Score += value;
+        ScoreText.text = Score.ToString();
     }
 
     public void AddRage(int value)
     {
         RageMeter += value;
+        RageText.text = RageMeter.ToString();
     }
 
     public void CalmRage(int value)
     {
         RageMeter -= value;
+        RageText.text = RageMeter.ToString();
     }
 }
