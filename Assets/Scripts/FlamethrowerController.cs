17d0f05e8c3a57e9c5c8e34bfb2f4a7a7c038a3e
jcwrightson <jcwrightson@gmail.com>
2022-06-08 16:39:12 +0100

    flamethrower Mk.1

---
+++ Assets/Scripts/FlamethrowerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlamethrowerController : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionReference actionRef;
+
+    ParticleSystem system
+    {
+        get
+        {
+            if (_CachedSystem == null)
+                _CachedSystem = GetComponent<ParticleSystem>();
+            return _CachedSystem;
+        }
+    }
+
+
+
+    private ParticleSystem _CachedSystem;
+
+    AudioSource audioData;
+
+    void Start()
+    {
+        actionRef.action.performed += OnTrigger;
+
+        // audioData = GetComponent<AudioSource>();
+        if (system.isEmitting)
+        {
+            // audioData.Play(0);
+        }
+    }
+
+    private void OnTrigger(InputAction.CallbackContext obj)
+    {
+        if (system.isEmitting)
+        {
+            system.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            // audioData.Stop();
+            // GetComponent<Collider>().enabled = false;
+            return;
+        }
+        system.Play(true);
+        // audioData.Play(0);
+        // GetComponent<Collider>().enabled = true;
+    }
+}
